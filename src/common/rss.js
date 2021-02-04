2acfd0f9ee28b8701c26a7c8a5f955792e07e609
Kouki-Kubota <kubou1115@outlook.jp>
2021-02-05 05:12:25 +0900

    axios

--- src/common/rss.js
+++ src/common/rss.js
@@ -1,22 +1,33 @@
-const URL = "https://news.yahoo.co.jp/rss/categories/sports.xml";
+export default {
+  
+  /* let xhr=new XMLHttpRequest();
+  
+  xhr.open(`GET`,URL);
+  xhr.responseType=`document`;
+  xhr.send();
+  
+  xhr.onload=()=>{
+    rss_data=xhr.response;
+  } */
+  mounted() {
+  const URL = "https://news.yahoo.co.jp/rss/categories/sports.xml"
+  
+  fetch(URL, {
+    method: "GET",
+  }).then(response => response.text())
+  .then(text => {
+    rss_data=text;
+  });
+  //記事を取得
+  items = rss_data.getElementsByTagName(`item`),
+  //記事のタイトルを取得
+  title = items[0].getElementsByTagName(`title`).textContent,
+  date = new Date(items[0].getElementsByTagName(`pubDate`).textContent),
+  
+  content_text = items[0].getElementsByTagName(`content`).textContent,//contentの内容を取得
+  buffer_dom = document.createElement(`div`),//バッファ的な要素の作成
+  //buffer_dom.innerHTML=content_text, //contentの内容を元にバッファ要素にdomを構築
+  thumbnail = buffer_dom.getElementsByTagName(`img`)[0].src //最初のimgタグのsrcを取得
+  }
+}
 
-let xhr=new XMLHttpRequest();
-
-xhr.open(`GET`,URL);
-xhr.responseType=`document`;
-xhr.send();
-
- let rss_data = xhr.onload=()=>{
-  rss_data=xhr.response;
-};
-
-//記事を取得
-let items= rss_data.getElementsByTagName(`item`);
-//記事のタイトルを取得
-let title=items[0].getElementsByTagName(`title`).textContent;
-let date=new Date(items[0].getElementsByTagName(`pubDate`).textContent);
-
-let content_text=items[0].getElementsByTagName(`content`).textContent; //contentの内容を取得
-let buffer_dom=document.createElement(`div`); //バッファ的な要素の作成
-buffer_dom.innerHTML=content_text; //contentの内容を元にバッファ要素にdomを構築
-let thumbnail=buffer_dom.getElementsByTagName(`img`)[0].src; //最初のimgタグのsrcを取得
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
   <ul>
     <li>
       <v-card id="card" elevation="2">
-      <v-card-title>例</v-card-title>
+      <v-card-title>{{ title }}</v-card-title>
       <v-card-subtitle>ほにゃらら</v-card-subtitle>
       <v-img
         lazy-src="https://picsum.photos/id/11/10/6"
@@ -18,31 +18,30 @@
 </template>
 
 <script>
-  /* export default {
-    mounted() {
-    const URL = "https://news.yahoo.co.jp/rss/categories/sports.xml";
-
-    let xhr=new XMLHttpRequest();
-
-    xhr.open(`GET`,URL);
-    xhr.responseType=`document`;
-    xhr.send();
-
-    let rss_data = xhr.onload=()=>{
-      rss_data=xhr.response;
-    }
-    //記事を取得
-    let items= rss_data.getElementsByTagName(`item`);
-    //記事のタイトルを取得
-    let title=items[0].getElementsByTagName(`title`).textContent;
-    let date=new Date(items[0].getElementsByTagName(`pubDate`).textContent);
-
-    let content_text=items[0].getElementsByTagName(`content`).textContent; //contentの内容を取得
-    let buffer_dom=document.createElement(`div`); //バッファ的な要素の作成
-    buffer_dom.innerHTML=content_text; //contentの内容を元にバッファ要素にdomを構築
-    let thumbnail=buffer_dom.getElementsByTagName(`img`)[0].src; //最初のimgタグのsrcを取得
+  export default {
+  mounted() {
+  const URL = "https://news.yahoo.co.jp/rss/categories/sports.xml"
+  
+  fetch(URL, {
+    method: "GET",
+  }).then(response => response.text())
+  .then(text => {
+    rss_data=text;
+  });
+  //記事を取得
+  let items = rss_data.getElementsByTagName(`item`)
+  //記事のタイトルを取得
+  let title = items[0].getElementsByTagName(`title`).textContent
+  let date = new Date(items[0].getElementsByTagName(`pubDate`).textContent)
+  
+  let content_text = items[0].getElementsByTagName(`content`).textContent//contentの内容を取得
+  let buffer_dom = document.createElement(`div`)//バッファ的な要素の作成
+  buffer_dom.innerHTML=content_text //contentの内容を元にバッファ要素にdomを構築
+  let thumbnail = buffer_dom.getElementsByTagName(`img`)[0].src //最初のimgタグのsrcを取得
   }
-} */
+}
+
+
       
 </script>
 
